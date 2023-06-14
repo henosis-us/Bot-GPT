21279f3aa49006e5f983b4eee6d5077e104ed9fe
kcy12 <matthewman1121@outlook.com>
2023-06-13 23:13:17 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,131 @@
+from utilties.Openai_Utils import generate_response
+import io
+import discord
+import openai
+import asyncio
+import textwrap
+from utilties.vars import OPENAI_API_KEY, DISCORD_TOKEN
+from discord.ext import commands
+import aiohttp
+from utilties.tokenizer import num_tokens_from_string
+# Set up OpenAI API
+openai.api_key = OPENAI_API_KEY
+
+# Set up Discord bot
+intents = discord.Intents.default()
+intents.message_content = True
+intents.presences - True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+async def send_text_or_file(ctx, generated_text):
+    max_width = 150
+    def reformat_text(text, max_width=max_width):  # Adjusted max_width for mobile users
+        return '\n'.join(textwrap.wrap(text, width=max_width))
+
+    if len(generated_text) <= 2000:
+        mention = ctx.author.mention  # Mention the user who sent the command
+        await ctx.send(f"{mention} {generated_text}")
+    else:
+        # Reformat the generated_text only if it's sent as a file
+        reformatted_text = reformat_text(generated_text)
+
+        with io.StringIO(reformatted_text) as temp_file:
+            temp_file.name = 'generated_text.txt'
+            mention = ctx.author.mention  # Mention the user who sent the command
+            await ctx.send(f"{mention} Here is the generated text:", file=discord.File(temp_file))
+    
+async def read_txt_file(file_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_url) as response:
+            if response.status == 200:
+                text = await response.text()
+                return text
+            else:
+                return None
+
+async def handle_prompt(ctx, prompt):
+    if ctx.message.attachments:
+        attachment = ctx.message.attachments[0]
+        if attachment.filename.endswith('.txt') or attachment.filename.endswith('.py'):
+            file_url = attachment.url
+            file_prompt = await read_txt_file(file_url)
+            if file_prompt is None:
+                await ctx.send(f"{ctx.author.mention} Failed to read the attached file.")
+                return
+            prompt = f"{prompt}\n{file_prompt}" if prompt else file_prompt
+        else:
+            await ctx.send(f"{ctx.author.mention} Please attach a .txt or .py file.")
+            return
+    return prompt
+
+@bot.command(name='gpt3', help='cheapest model essentially free to run, great for simple reasoning or simple tasks/generations, can intake around 4k tokens')
+async def gpt3(ctx, *, prompt: str = None):
+    prompt = await handle_prompt(ctx, prompt)
+    if prompt is None:
+        return
+    generated_text = await generate_response("gpt-3.5-turbo", prompt)
+    print(generated_text)
+    await send_text_or_file(ctx, generated_text)
+
+        
+@bot.command(name='gpt3long', help='minor cost increase with no increase in reasoning but can intake around 16k tokens')
+async def gpt3(ctx, *, prompt: str = None):
+    prompt = await handle_prompt(ctx, prompt)
+    if prompt is None:
+        return
+    generated_text = await generate_response("gpt-3.5-turbo-16k", prompt)
+    print(generated_text)
+    await send_text_or_file(ctx, generated_text)
+
+@bot.command(name='gpt4', help='10x cost but much better in reasoning tasks, can intake about 8k tokens')
+async def gpt4(ctx, *, prompt: str = None):
+    prompt = await handle_prompt(ctx, prompt)
+    if prompt is None:
+        return
+    generated_text = await generate_response("gpt-4", prompt)
+    print(generated_text)
+    await send_text_or_file(ctx, generated_text)
+        
+@bot.command(name='gptthread')
+async def gptthread(ctx):
+    thread = await ctx.channel.create_thread(name=f"GPT Chat with {ctx.author.display_name}", type=discord.ChannelType.public_thread)
+    await thread.send(f"{ctx.author.mention} You have started a chat with GPT. Type your messages in this thread using the gpt3t/gpt4t command to consider up to 3k tokens of context\n you may still use gpt4 & gpt3 to get single shot of a specific prompt")
+
+@bot.command(name='privatethread', help='creates a private thread, likely this can be seen by admins')
+async def privatethread(ctx):
+    thread = await ctx.channel.create_thread(name=f"Private GPT Chat with {ctx.author.display_name}", type=discord.ChannelType.private_thread)
+    await thread.send(f"{ctx.author.mention} You have started a private chat with GPT. Type your messages in this thread using the gpt3t/gpt4t command to consider up to 3k tokens of context\n you may still use gpt4 & gpt3 to get single shot of a specific prompt")
+
+@bot.command(name='gpt')
+async def gpt(ctx, *, prompt: str):
+    if ' ' in prompt:
+        mention = ctx.author.mention  # Mention the user who sent the command
+        await ctx.send(f"{mention} You accidentally used a space :D")
+    else:
+        mention = ctx.author.mention  # Mention the user who sent the command
+        await ctx.send(f"{mention} Your prompt doesn't contain a space.")
+
+@bot.command(name='tokens', help='rough token count using gpt4 chat spec')
+async def tokens(ctx, *, message: str = None):
+    if ctx.message.attachments:
+        attachment = ctx.message.attachments[0]
+        if attachment.filename.endswith('.txt'):
+            file_url = attachment.url
+            file_text = await read_txt_file(file_url)
+            if file_text is None:
+                await ctx.send(f"{ctx.author.mention} Failed to read the attached file.")
+                return
+            
+            message = file_text
+        else:
+            await ctx.send(f"{ctx.author.mention} Please attach a .txt file.")
+            return
+    num_tokens = num_tokens_from_string(message, "cl100k_base")
+    await ctx.send(f'The message contains {num_tokens} tokens.')
+
+if __name__ == '__main__':
+    bot.run(DISCORD_TOKEN)
---
+++ utilties/Openai_Utils.py
@@ -0,0 +1,24 @@
+# openai_utils.py
+import openai
+import asyncio
+
+async def generate_response(model_name, prompt):
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": "You are chatting with an AI trained by OpenAI."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=2000,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+
+        generated_text = response['choices'][0]['message']['content'].strip()
+        return generated_text
+
+    except openai.error.RateLimitError as e:
+        asyncio.sleep(3)  # Wait for a few seconds before retrying
+        return await generate_response(model_name, prompt)
